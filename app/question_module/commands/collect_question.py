1c5b44ef034d9879e2919e501ef59e8c88cfd707
Vincent <k2r79@hotmail.fr>
2014-12-06 12:02:02 +0100

    Integrated HAL support (dougrain) and HTTP Mocking (httpretty)

--- app/question_module/commands/collect_question.py
+++ app/question_module/commands/collect_question.py
@@ -1,3 +1,4 @@
+import dougrain
 from flask.ext.script import Command, Option
 from app.question_module.exceptions.request_exceptions import WrongStatusCodeException, NoContentException
 from pip.backwardcompat import raw_input
@@ -7,6 +8,8 @@ import requests
 class CollectQuestion(Command):
     """Collects the latest question on the question server"""
 
+    url_format = "http://%s/%s"
+
     option_list = (
         Option('-h', '--host', help='Question server address'),
         Option('-u', '--url', help='Path to the question'),
@@ -15,15 +18,22 @@ class CollectQuestion(Command):
     def run(self, host, url):
         print("Connecting to server...")
         question = self.request_question(host, url)
-        answer = raw_input(question)
+        answer = raw_input(question.properties["question"])
 
-    @staticmethod
-    def request_question(host, url):
-        response = requests.get(host + "/" + url)
+    def request_question(self, host, url):
+        response = requests.get(self.url_format % (host, url))
 
         if response.status_code == 204:
             raise NoContentException()
         elif response.status_code != 200:
             raise WrongStatusCodeException(response.status_code)
 
-        return response.content
+        return dougrain.Document.from_object(response.json())
+
+    def post_answer(self, host, answer_url):
+        pass
+
+    def create_answer_request(self, host, answer_url):
+        request = requests.post(self.url_format % (host, answer_url))
+
+        return request
--- tests/question_module/commands/collect_question_tests.py
+++ tests/question_module/commands/collect_question_tests.py
@@ -1,43 +1,63 @@
+import json
 import unittest
+from dougrain import Builder
 import expert_system
-import mock
+import httpretty
 from app.question_module.commands.collect_question import CollectQuestion
 from app.question_module.exceptions import request_exceptions
-import requests
 
 
 class TestCollectQuestion(unittest.TestCase):
-    host = "127.0.0.1"
+    host = "test.com"
     url = "questions/latest"
+    answer_url = "question/%d/answer"
 
     def setUp(self):
         expert_system.app.config['TESTING'] = True
         self.app = expert_system.app.test_client()
         self.collect_question_command = CollectQuestion()
 
+    def tearDown(self):
+        httpretty.reset()
+        httpretty.disable()
+
+    # Question request
+    @httpretty.activate
     def test_request_question_found(self):
-        response = requests.Response()
-        response.status_code = 200
-        response._content = "Texte de la question"
-        requests.get = mock.Mock(return_value=response)
+        request_body = Builder(self.url).set_property("id", 1) \
+                                        .set_property("question", "Texte de la question")
 
-        self.assertEquals("Texte de la question", self.collect_question_command.request_question(self.host, self.url))
+        httpretty.register_uri(httpretty.GET, "http://test.com/questions/latest",
+                               body=json.dumps(request_body.as_object()),
+                               content_type="application/hal+json")
 
+        response = self.collect_question_command.request_question(self.host, self.url)
+
+        self.assertEquals(1, response.properties["id"])
+        self.assertEquals("Texte de la question", response.properties["question"])
+
+    @httpretty.activate
     def test_request_question_not_found(self):
-        response = requests.Response()
-        response.status_code = 404
-        requests.get = mock.Mock(return_value=response)
+        httpretty.register_uri(httpretty.GET, "http://test.com/questions/latest",
+                               status=404)
 
         self.assertRaises(request_exceptions.WrongStatusCodeException,
                           lambda: self.collect_question_command.request_question(self.host, self.url))
 
+    @httpretty.activate
     def test_request_question_but_no_question(self):
-        response = requests.Response()
-        response.status_code = 204
-        requests.get = mock.Mock(return_value=response)
+        httpretty.register_uri(httpretty.GET, "http://test.com/questions/latest",
+                               status=204)
 
         self.assertRaises(request_exceptions.NoContentException,
                           lambda: self.collect_question_command.request_question(self.host, self.url))
+
+    # Answer posting
+    # @httpretty.activate
+    # def test_post_answer(self):
+    # request = self.collect_question_command.post_answer(self.host, self.answer_url)
+    #
+    #     self.assertEquals("POST", request.method)
 
 
 if __name__ == '__main__':
