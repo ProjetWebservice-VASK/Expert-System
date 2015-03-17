031952890b2ba9372354f91733b27909398396a4
Vincent Kelleher <k2r79@hotmail.fr>
2015-03-17 11:07:54 +0100

    New redirection code for the latest question volatile URL

--- tests/question_module/commands/collect_question_tests.py
+++ tests/question_module/commands/collect_question_tests.py
@@ -16,12 +16,13 @@ class TestCollectQuestion(unittest.TestCase):
     url_format = "http://%s/%s"
     host = "test.com"
 
-    question_url = "questions/next"
+    latest_question_url = "questions/next"
     received_question_url = "questions/%d/received"
 
     answer_url = "question/%d/answer"
     unknown_answer_url = "question/%d/answer"
 
+    question_url = "questions/%d"
     question_id = 1
     question_text = "Texte de la question"
     answer_text = "Texte de la réponse"
@@ -38,20 +39,27 @@ class TestCollectQuestion(unittest.TestCase):
     # Question request
     @httpretty.activate
     def test_request_question_found(self):
-        request_body = dougrain.Builder(self.question_url)\
+        request_body = dougrain.Builder(self.latest_question_url)\
             .set_property("id", self.question_id) \
             .set_property("question", self.question_text) \
             .add_link("received", self.received_question_url % self.question_id) \
             .add_link("answer", self.answer_url % self.question_id)
 
-        httpretty.register_uri(httpretty.GET, self.url_format % (self.host, self.question_url),
+        # Latest question volatile url
+        httpretty.register_uri(httpretty.GET, self.url_format % (self.host, self.latest_question_url),
+                               location=self.url_format % (self.host, self.question_url % self.question_id),
+                               status=307)
+
+        # Question resource url
+        httpretty.register_uri(httpretty.GET, self.url_format % (self.host, self.question_url % self.question_id),
                                body=json.dumps(request_body.as_object()),
                                content_type="application/hal+json")
 
+        # Well received question url
         httpretty.register_uri(httpretty.POST, self.url_format % (self.host, self.received_question_url % self.question_id),
                                status=204)
 
-        response = self.collect_question_command.request_question(self.host, self.question_url)
+        response = self.collect_question_command.request_question(self.host, self.latest_question_url)
 
         self.assertEquals(self.question_id, response.properties["id"])
         self.assertEquals(self.question_text, response.properties["question"])
@@ -75,7 +83,7 @@ class TestCollectQuestion(unittest.TestCase):
 
     # Question format
     def test_is_response_format_valid_with_valid_response(self):
-        response = dougrain.Builder(self.question_url)\
+        response = dougrain.Builder(self.latest_question_url)\
             .set_property("id", self.question_id) \
             .set_property("question", self.question_text) \
             .add_link("received", self.received_question_url % self.question_id) \
@@ -84,7 +92,7 @@ class TestCollectQuestion(unittest.TestCase):
         self.assertTrue(self.collect_question_command.is_response_format_valid(dougrain.Document.from_object(response)))
 
     def test_is_response_format_valid_with_invalid_response(self):
-        response = dougrain.Builder(self.question_url) \
+        response = dougrain.Builder(self.latest_question_url) \
             .set_property("question", self.question_text) \
             .add_link("received", self.received_question_url % self.question_id) \
             .add_link("answer", self.answer_url % self.question_id).as_object()
@@ -93,31 +101,31 @@ class TestCollectQuestion(unittest.TestCase):
 
     @httpretty.activate
     def test_request_question_not_found(self):
-        httpretty.register_uri(httpretty.GET, self.url_format % (self.host, self.question_url),
+        httpretty.register_uri(httpretty.GET, self.url_format % (self.host, self.latest_question_url),
                                status=404)
 
         self.assertRaises(request_exceptions.WrongStatusCodeException,
-                          lambda: self.collect_question_command.request_question(self.host, self.question_url))
+                          lambda: self.collect_question_command.request_question(self.host, self.latest_question_url))
 
     @httpretty.activate
     def test_request_question_but_no_question(self):
-        httpretty.register_uri(httpretty.GET, self.url_format % (self.host, self.question_url),
+        httpretty.register_uri(httpretty.GET, self.url_format % (self.host, self.latest_question_url),
                                status=204)
 
-        self.assertEquals(None, self.collect_question_command.request_question(self.host, self.question_url))
+        self.assertEquals(None, self.collect_question_command.request_question(self.host, self.latest_question_url))
 
     @httpretty.activate
     def test_request_question_wrong_page(self):
-        request_body = dougrain.Builder(self.question_url).set_property("id", self.question_id) \
+        request_body = dougrain.Builder(self.latest_question_url).set_property("id", self.question_id) \
             .set_property("dog", "woof") \
             .add_link("cat", "meow")
 
-        httpretty.register_uri(httpretty.GET, self.url_format % (self.host, self.question_url),
+        httpretty.register_uri(httpretty.GET, self.url_format % (self.host, self.latest_question_url),
                                body=json.dumps(request_body.as_object()),
                                content_type="application/hal+json")
 
         self.assertRaises(request_exceptions.InvalidQuestionFormatException,
-                          lambda: self.collect_question_command.request_question(self.host, self.question_url))
+                          lambda: self.collect_question_command.request_question(self.host, self.latest_question_url))
 
     # Answer creation
     def test_create_answer_request(self):
